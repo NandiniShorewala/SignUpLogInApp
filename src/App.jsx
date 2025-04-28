7d9255e6f08357c0aeaa2080185e5096291ae237
Nandini Shorewala <nandinishorewala29@gmail.com>
2025-04-28 22:19:09 +0530

    New Project created

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import LoginSignup from './components/LoginSignup';
+
+const App = () => {
+  return (
+    <div>
+      <LoginSignup/>
+    </div>
+  )
+}
+
+export default App
